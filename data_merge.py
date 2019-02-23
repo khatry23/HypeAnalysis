73e3f67723d5e92318dd1ad3c912df8ac4751ce6
khatry23 <43261810+khatry23@users.noreply.github.com>
2019-02-23 15:19:55 -0500

    Update data_merge.py

--- data_merge.py
+++ data_merge.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import re
 import sys
-file_shreya = open("/Users/shravspy/Documents/Sajan/movie_review.txt","r")
-file_sajan = open("/Users/shravspy/Documents/Sajan/saved_tweets.txt","r")
-output_file = open("/Users/shravspy/Documents/Sajan/output_MR.txt", "w")
+file_sh = open("movie_review.txt","r")
+file_sa = open("saved_tweets.txt","r")
+output_file = open("output_MR.txt", "w")
 
-for line in file_shreya:
-	shreya_list=line.strip().split(",",1)
-	for line in file_sajan:
-		sajan_list=line.strip().split(",",1)
-		if shreya_list[0]==sajan_list[0]:
-			data=shreya_list[0]+','+shreya_list[1]+':'+sajan_list[1]
+for line in file_sh:
+	sh_list=line.strip().split(",",1)
+	for line in file_sa:
+		sa_list=line.strip().split(",",1)
+		if sh_list[0]==sa_list[0]:
+			data=sh_list[0]+','+sh_list[1]+':'+sa_list[1]
 			output_file.write(data+"\n")
 output_file.close() 
 	
-file_shreya.close()
-file_sajan.close()
+file_sh.close()
+file_sa.close()
 
